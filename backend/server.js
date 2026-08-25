c5582c01efd9c25a51418fb2e435a30b6c06b83f
ishanjain06 <ishan.jain062004@gmail.com>
2026-08-26 00:30:41 +0530

    Handle invalid channel ids

--- backend/server.js
+++ backend/server.js
@@ -341,10 +341,30 @@ app.get("/api/channels/mine", auth, async (req, res) => {
 });
 
 app.get("/api/channels/:id", async (req, res) => {
-  const channel = await Channel.findById(req.params.id).populate(
-    "owner",
-    "username avatar"
-  );
+  try {
+    const channel = await Channel.findById(req.params.id).populate(
+      "owner",
+      "username avatar"
+    );
+
+    if (!channel) {
+      return res.status(404).json({
+        message: "Channel not found.",
+      });
+    }
+
+    res.json({
+      channel,
+      videos: await Video.find({
+        channel: channel._id,
+      }).sort({ createdAt: -1 }),
+    });
+  } catch {
+    res.status(400).json({
+      message: "Invalid channel id.",
+    });
+  }
+});
 
   if (!channel) {
     return res.status(404).json({
